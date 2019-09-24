5a0a317a00e222bd8452549588b6b568c3904d54
SaroshKhawar496 <sarosh.496@gmail.com>
2019-09-24 18:45:31 -0400

    Added a form to add new to do item to list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Todos from "./Todos";
+import AddToDo from "./AddToDo";
 
 //using materialized css included in index.h
 class App extends Component {
@@ -16,11 +17,19 @@ class App extends Component {
       todos: todos
     });
   };
+  addTodo = todo => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos
+    });
+  };
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddToDo addTodo={this.addTodo} />
       </div>
     );
   }
